a4c68fb5b163323a45ca661810c7f306fcba4ba0
louloupM <89186866+louloupM@users.noreply.github.com>
2022-05-10 20:18:03 +0200

    Update what-s-up-science.py

--- what-s-up-science.py
+++ what-s-up-science.py
@@ -56,6 +56,7 @@ if data is not None:
     titles = library['Title'].to_list()    
     titles = [each_string.lower() for each_string in titles]
     titles = [element.split() for element in titles]
+    st.write(titles)
     titles = ' '.join(str(e) for e in titles)
     
     
