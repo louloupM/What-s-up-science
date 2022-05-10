0d749aa66ce0d86c26583086e446e0df4be20376
louloupM <89186866+louloupM@users.noreply.github.com>
2022-05-10 16:56:58 +0200

    Update what-s-up-science.py

--- what-s-up-science.py
+++ what-s-up-science.py
@@ -54,10 +54,9 @@ row2 = st.container()
 if data is not None:    
     library = pd.read_csv(data)
     st.write(library)
-    library['Title'] = [re.sub(r'[^\w\s]',' ',word) for word in library['Title']]
     titles = library['Title'].to_list()    
+    titles = [re.sub(r'[^\w\s]',' ',word) for word in {titles}]
     titles = str(titles).split()
-    
     titles = [each_string.lower() for each_string in titles]
     
     titles = [word for word in titles if word not in stopwords.words('english')]
