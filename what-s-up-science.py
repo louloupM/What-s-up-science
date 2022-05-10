a7352e5cd41d4ef26f3293f666d23b436e582620
louloupM <89186866+louloupM@users.noreply.github.com>
2022-05-10 12:03:42 +0200

    Update what-s-up-science.py

--- what-s-up-science.py
+++ what-s-up-science.py
@@ -70,8 +70,8 @@ if data is not None:
     x = df.loc[:20,'A'].values
     y = df.loc[:20,'B'].values
     plt.gca().invert_yaxis()
-    plt.tick_params(axis='x', labelsize=14)
-    plt.tick_params(axis='y', labelsize=14)
+    plt.tick_params(axis='x', labelsize=12)
+    plt.tick_params(axis='y', labelsize=12)
     plt.barh(x, y, height=0.5, label = 'Bar', color = 'lightskyblue')
 
     row1col3.pyplot()
