739ce62ead8b21037e46e03d13934dcb46fcde47
louloupM <89186866+louloupM@users.noreply.github.com>
2022-05-12 10:33:38 +0200

    Update what-s-up-science.py

--- what-s-up-science.py
+++ what-s-up-science.py
@@ -59,7 +59,7 @@ if data is not None:
     titles = [re.sub(r'\([^)]*\)', '',str(word)) for word in titles]
 
 
-    titles = [re.sub(r'\(|\)\'',' ',str(word)) for word in titles]
+    titles = [re.sub(r'\(|\)\'',' ',str(word),flags=re.MULTILINE) for word in titles]
     titles = [word for word in titles if word not in stopwords.words('english')]
     titles = Counter(titles)
     
