ee2096cb129c53c1618d6e97d0c8aa033037a0d7
louloupM <89186866+louloupM@users.noreply.github.com>
2022-05-12 20:50:07 +0200

    Update what-s-up-science.py

--- what-s-up-science.py
+++ what-s-up-science.py
@@ -49,7 +49,7 @@ graph_pie = st.container()
 journals_list = st.container()
 row1col1, row1col2, row1col3 = st.columns([7,4.7,7])
 row2 = st.container()
-row3col1, row3col2, row3col3 = st.columns([6,1,6])
+row3col1, row3col2, row3col3 = st.columns([6,1,3])
 
  
     
