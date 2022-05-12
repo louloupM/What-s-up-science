c84b986851c5fb870a80b6ee13679cb6beb8d4a0
louloupM <89186866+louloupM@users.noreply.github.com>
2022-05-12 19:38:52 +0200

    Update what-s-up-science.py

--- what-s-up-science.py
+++ what-s-up-science.py
@@ -108,8 +108,8 @@ if data is not None:
 
 
     for element in df['A'].tolist():
-        if df_journal_list[df_journal_list['Title']==str(element)]:
-            st.write('good')
+        if df_journal_list['Title'].str.contains('element'):
+            print ("Mel is there")
 
     
     
