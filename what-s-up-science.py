c89a4afc273323111ade0ff9e4ca03d4f3bb2da5
louloupM <89186866+louloupM@users.noreply.github.com>
2022-05-12 20:54:30 +0200

    Update what-s-up-science.py

--- what-s-up-science.py
+++ what-s-up-science.py
@@ -101,7 +101,11 @@ if data is not None:
     plt.barh(x2, y2, label = 'Bar', color = 'lightskyblue')
     row1col3.pyplot(use_container_width=True,)
     
-    row2col2.markdown("<h1 style='text-align: center;font-style: italic;font-size:20px; color: Black;'>"+str(citation)+" total citations</h1>", unsafe_allow_html=True)   
+    with row2col2:
+        st.write('')
+        st.write('')
+        st.write('')
+        st.markdown("<h1 style='text-align: center;font-style: italic;font-size:20px; color: Black;'>"+str(citation)+" total citations</h1>", unsafe_allow_html=True)   
     
     
     #Publisher code
