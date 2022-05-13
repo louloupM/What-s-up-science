ab49c690ef2d31ff3403a9bfa87b9c8ce5973698
louloupM <89186866+louloupM@users.noreply.github.com>
2022-05-13 12:34:51 +0200

    Update what-s-up-science.py

--- what-s-up-science.py
+++ what-s-up-science.py
@@ -204,6 +204,7 @@ if data is not None:
     df = pd.DataFrame(publishers_occurence)
     df.columns = ['Publisher', 'Occurence']
     df.sort_values('Occurence')
+    st.write(df)
     df.groupby(['Publisher']).sum().plot(kind='pie', radius = 1, subplots=True, legend= True, ylabel='',labeldistance=None, fontsize=10, figsize=(10,10),colormap='Set3')
     plt.legend(loc='upper right', fontsize=12)
     plt.margins(0,0)
