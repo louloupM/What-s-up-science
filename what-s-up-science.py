bafdc1d8389ed6fc2c54db96bdaacc7026925a4e
louloupM <89186866+louloupM@users.noreply.github.com>
2022-05-13 10:47:19 +0200

    Update what-s-up-science.py

--- what-s-up-science.py
+++ what-s-up-science.py
@@ -147,7 +147,7 @@ if data is not None:
 
 
     fig = px.scatter_geo(df,  locations="iso_alpha",
-                     size='Pop', size_max = 25, color='country', 
+                     size='Pop', size_max = 25, color='Country', 
                      projection="natural earth")
 
 
