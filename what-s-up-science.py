387715ca3e49ed4f0c03016c60d12e3200be176c
louloupM <89186866+louloupM@users.noreply.github.com>
2022-05-13 08:20:28 +0200

    Update what-s-up-science.py

--- what-s-up-science.py
+++ what-s-up-science.py
@@ -132,7 +132,7 @@ if data is not None:
     publishers_occurence = Counter(publishers).most_common(10)
     df = pd.DataFrame(publishers_occurence)
     df.columns = ['Publisher', 'Occurence']
-    df.groupby(['Publisher']).sum().plot(kind='pie', subplots=True, labels=None, legend= True, ylabel='', fontsize=10, figsize=(9,9),colormap='Set3')
+    df.groupby(['Publisher']).sum().plot(kind='pie', subplots=True, labels=labels, legend= True, ylabel='', fontsize=10, figsize=(9,9),colormap='Set3')
 
     row2col1.pyplot(use_container_width=True,)
      
