396834729b8c7e7f15cfaa4277a5549c8ebfbc7d
louloupM <89186866+louloupM@users.noreply.github.com>
2022-05-13 10:52:10 +0200

    Update what-s-up-science.py

--- what-s-up-science.py
+++ what-s-up-science.py
@@ -178,6 +178,7 @@ if data is not None:
     df.groupby(['Publisher']).sum().plot(kind='pie',radius = 1, subplots=True, legend= True, ylabel='',labeldistance=None, fontsize=10, figsize=(10,10),colormap='Set3')
     plt.legend(loc='upper left', fontsize=11)
     plt.margins(0,0)
+    row2col1.write("lol")
     row2col1.pyplot()
      
     #Domain code
