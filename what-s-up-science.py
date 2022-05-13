c818d5624282c84c355d742e244924797e272194
louloupM <89186866+louloupM@users.noreply.github.com>
2022-05-13 11:40:37 +0200

    Update what-s-up-science.py

--- what-s-up-science.py
+++ what-s-up-science.py
@@ -179,7 +179,7 @@ if data is not None:
     df.groupby(['Publisher']).sum().plot(kind='pie',radius = 1, subplots=True, legend= True, ylabel='',labeldistance=None, fontsize=10, figsize=(10,10),colormap='Set3')
     plt.legend(loc='upper left', fontsize=11)
     plt.margins(0,0)
-    row3col1.markdown("<h2 style='text-align: center; font-size:30px; color: White;'>Blank</h2>", unsafe_allow_html=True)
+    row3col1.markdown("<h2 style='text-align: center; font-size:10px; color: White;'>Blank</h2>", unsafe_allow_html=True)
     row3col1.pyplot()
      
     #Domain code
@@ -205,5 +205,5 @@ if data is not None:
     df.groupby(['Publisher']).sum().plot(kind='pie', radius = 1, subplots=True, legend= True, ylabel='',labeldistance=None, fontsize=10, figsize=(10,10),colormap='Set3')
     plt.legend(loc='upper right', fontsize=11)
     plt.margins(0,0)
-    row3col3.markdown("<h2 style='text-align: center; font-size:30px; color: White;'>Blank</h2>", unsafe_allow_html=True)
+    row3col3.markdown("<h2 style='text-align: center; font-size:10px; color: White;'>Blank</h2>", unsafe_allow_html=True)
     row3col3.pyplot()  
